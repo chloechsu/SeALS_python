878ddb8f551da6e9dee85f8fffa1b6d801c8acd7
Chloe Hsu <chingyunhsu22@gmail.com>
2018-04-14 14:09:06 -0700

    include representation of 0 as a tensor

--- SeALS/tensors/cp_tensor.py
+++ SeALS/tensors/cp_tensor.py
@@ -2,54 +2,6 @@ import tensorly as tl
 import numpy as np
 from copy import deepcopy
 
-class CPTensorOperator:
-    """ A wrapper around CPTensor to represent operators.
-        If A is a rank R tensor operator mapping tensors of dim I1 x ... x In to
-        tensors of dim I1' x ... x In',
-        the corresponding CPTensor object has dim (I1' x I1) x ... x (In' x In).
-    """
-    def __init__(self, factors, lambdas=None):
-        try:
-            self.rank = factors[0].shape[2]
-        except IndexError:
-            print "factors must be a list of 3D arrays"
-            raise IndexError
-
-        factors_flat = []
-        self.dim = []
-        for f in factors:
-            assert len(f.shape) == 3 and f.shape[2] == self.rank, \
-                "factors must be a list of 3D arrays of dimension I_d' x I_d x R"
-            self.dim.append((f.shape[0], f.shape[1]))
-            factors_flat.append(f.reshape(f.shape[0] * f.shape[1], self.rank))
-
-        self.tensor = CPTensor(factors_flat, lambdas)
-
-    def multiply(self, B):
-        """ B must be a tensor of dim I1 x ... x In
-            The multiplication results in a tensor of dim I1' x ... x In'
-        """
-        assert isinstance(B, CPTensor), "wrong type, not a CPTensor"
-        assert np.array_equal([t[1] for t in self.dim], B.dim), \
-            "dimensions do not match"
-        factors = []
-        for d in xrange(len(self.dim)):
-            f = np.zeros((self.dim[d][0], self.rank * B.rank))
-            for i in range(self.rank):
-                operator = self.tensor.factors[d][:,i].reshape(self.dim[d]),
-                f[:, i*B.rank:(i+1)*B.rank] = np.dot(operator, B.factors[d])
-            factors.append(f)
-        lambdas = np.outer(self.tensor.lambdas, B.lambdas).flatten()
-        return CPTensor(factors, lambdas)
-
-    def display(self):
-        print "CPTensorOperator with the following factors:"
-        for i, f in enumerate(self.tensor.factors):
-            print f.reshape(self.dim[i][0], self.dim[i][1])
-        print "and lambdas:"
-        print self.tensor.lambdas
-        print ""
-
 class CPTensor:
     """ A light wrapper around list of NDArrays to represent CP tensors.
         If G is a rank R tensor of dim I1 x I2 x ... x In,
@@ -57,6 +9,13 @@ class CPTensor:
         I1 x R, I2 x R, ... , In x R. 
     """
     def __init__(self, factors, lambdas=None):
+        # representation of a zero tensor
+        if factors == []:
+            self.rank = 0
+            self.lambdas = []
+            self.dim = []
+            return
+
         # use copy to make sure that a new list instance is created
         self.factors = deepcopy(factors)
         try:
@@ -129,6 +88,10 @@ class CPTensor:
     def plus(self, B):
         """ Computes self + B, where B is a CPTensor of the same dim """
         assert isinstance(B, CPTensor), "wrong type, not a CPTensor"
+        if B.rank == 0:
+            return self
+        if self.rank == 0:
+            return B
         assert np.array_equal(self.dim, B.dim), "tensor dimension mismatch"
         concat_factors = [np.concatenate(
             (self.factors[i], B.factors[i]), axis=1) \
@@ -146,6 +109,61 @@ class CPTensor:
             print f
         print "and lambdas:"
         print self.lambdas
+        print ""
+
+    def getNormalizationRatio(self):
+        """ Returns the ratio of the smallest normalization constant divided by
+        the largest normalization constant
+        """
+        self.arrange()
+        return self.lambdas[-1]/self.lambdas[0]
+
+class CPTensorOperator:
+    """ A wrapper around CPTensor to represent operators.
+        If A is a rank R tensor operator mapping tensors of dim I1 x ... x In to
+        tensors of dim I1' x ... x In',
+        the corresponding CPTensor object has dim (I1' x I1) x ... x (In' x In).
+    """
+    def __init__(self, factors, lambdas=None):
+        try:
+            self.rank = factors[0].shape[2]
+        except IndexError:
+            print "factors must be a list of 3D arrays"
+            raise IndexError
+
+        factors_flat = []
+        self.dim = []
+        for f in factors:
+            assert len(f.shape) == 3 and f.shape[2] == self.rank, \
+                "factors must be a list of 3D arrays of dimension I_d' x I_d x R"
+            self.dim.append((f.shape[0], f.shape[1]))
+            factors_flat.append(f.reshape(f.shape[0] * f.shape[1], self.rank))
+
+        self.tensor = CPTensor(factors_flat, lambdas)
+
+    def multiply(self, B):
+        """ B must be a tensor of dim I1 x ... x In
+            The multiplication results in a tensor of dim I1' x ... x In'
+        """
+        assert isinstance(B, CPTensor), "wrong type, not a CPTensor"
+        assert np.array_equal([t[1] for t in self.dim], B.dim), \
+            "dimensions do not match"
+        factors = []
+        for d in xrange(len(self.dim)):
+            f = np.zeros((self.dim[d][0], self.rank * B.rank))
+            for i in range(self.rank):
+                operator = self.tensor.factors[d][:,i].reshape(self.dim[d]),
+                f[:, i*B.rank:(i+1)*B.rank] = np.dot(operator, B.factors[d])
+            factors.append(f)
+        lambdas = np.outer(self.tensor.lambdas, B.lambdas).flatten()
+        return CPTensor(factors, lambdas)
+
+    def display(self):
+        print "CPTensorOperator with the following factors:"
+        for i, f in enumerate(self.tensor.factors):
+            print f.reshape(self.dim[i][0], self.dim[i][1])
+        print "and lambdas:"
+        print self.tensor.lambdas
         print ""
 
 def get_random_CP_tensor(dim, rank):
